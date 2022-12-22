797ad86d4ef0fe0c26793b8c06f4e998ef5fa54f
Trifan Grigore-Gabriel <trifangaby71@protonmail.com>
2022-12-22 09:49:33 +0200

    add sample product & txn

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,63 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import { PublicKey } from '@solana/web3.js';
+import { useWallet } from '@solana/wallet-adapter-react';
+import dynamic from "next/dynamic";
 import HeadComponent from '../components/Head';
+import Product from "../components/Product";
 
 // Constants
 const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-  
+    const WalletMultiButtonDynamic = dynamic(
+            async () =>
+      (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
+            { ssr: false }
+            );
+
+    // This will fetch the users' public key (wallet address) from any wallet we support
+    const { publicKey } = useWallet();
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        if (publicKey) {
+            fetch(`/api/fetchProducts`)
+        .then(response => response.json())
+        .then(data => {
+            setProducts(data);
+            console.log("Products", data);
+        });
+        }
+    }, [publicKey]);
+
+    const renderItemBuyContainer = () => (
+            <div className="products-container">
+                {products.map((product) => (
+                        <Product key={product.id} product={product} />
+                        ))}
+            </div>
+            );
+    const renderNotConnectedContainer = () => (
+            <div>
+                <div className="button-container">
+                    <WalletMultiButtonDynamic className="cta-button connect-wallet-button" />
+                </div>
+            </div>
+            );
   
   return (
     <div className="App">
       <HeadComponent/>
       <div className="container">
         <header className="header-container">
-          <p className="header"> 😳 Buildspace Emoji Store 😈</p>
-          <p className="sub-text">The only emoji store that accepts sh*tcoins</p>
+            <p className="header"> 🤨 <span className="gradient-text">Store</span></p>
+            <p className="sub-text">Accepting either 🤨 or SOL</p>
         </header>
 
-        <main>
-          <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
-        </main>
-
+          <main>
+              {publicKey ? renderItemBuyContainer() : renderNotConnectedContainer()}
+          </main>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src="twitter-logo.svg" />
           <a
